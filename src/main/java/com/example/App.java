3d5438677f2918665c43dddc9c116322b8f36c83
mostefa <mostefa.aliarous@gmail.com>
2024-04-19 15:45:35 +0100

    Strategy Pattern challenge solved

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -1,14 +1,16 @@
 package com.example;
 
 public class App {
-
+    
     public static void main(String[] args) {
-
+        
         var checkoutPage = new CheckoutPage();
 
-        checkoutPage.payWithCard();
-        checkoutPage.payByBankTransfer();
-
+//        checkoutPage.payWithCard();
+        checkoutPage.pay(PaymentMethode.payWithCard);
+//        checkoutPage.payByBankTransfer();
+        checkoutPage.pay(PaymentMethode.payByBankTransfer);
+        
     }
-
+    
 }
