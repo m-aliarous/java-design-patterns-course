545e58c35206015f251bb2f609400e2580bb8761
mostefa <mostefa.aliarous@gmail.com>
2024-04-19 10:57:12 +0100

    Observer pattern challenge solved

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -7,7 +7,10 @@ public class App {
         var location1 = new Location();
         var location2 = new Location();
         var weatherUpdates = new WeatherUpdates();
-
+        
+        location1.addPropertyChangeListener(weatherUpdates);
+        location2.addPropertyChangeListener(weatherUpdates);
+        
         location1.setWeatherEvent("Risk of flooding");
         location2.setWeatherEvent("Risk of drought");
 
--- src/main/java/com/example/WeatherUpdates.java
+++ src/main/java/com/example/WeatherUpdates.java
@@ -1,14 +1,21 @@
 package com.example;
 
+import java.beans.PropertyChangeEvent;
+import java.beans.PropertyChangeListener;
 import java.util.ArrayList;
 import java.util.List;
 
-public class WeatherUpdates {
+public class WeatherUpdates implements  PropertyChangeListener{
 
     private List<String> weatherUpdates = new ArrayList<>();
 
     public void printUpdates() {
         weatherUpdates.forEach(System.out::println);
+    }
+
+    @Override
+    public void propertyChange(PropertyChangeEvent pce) {
+        this.weatherUpdates.add((String)pce.getNewValue());
     }
 
 }
